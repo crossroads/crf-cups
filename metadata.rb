bab7e674c69438b477ef52c4caeb5ca6f06bbdee
Thomas Pike <thwpike@crossroads.org.hk>
2016-02-11 00:59:55 +0800

    Initial commit

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name             'crf-cups'
+maintainer       'Crossroads Foundation Ltd'
+maintainer_email 'itdept@crossroads.org.hk'
+license          'Apache 2.0'
+description      'Installs/Configures crf-cups'
+long_description 'Installs/Configures crf-cups'
+version          '0.1.0'
